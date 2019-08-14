4050279bb0151bf73260a057c9b635bcc2a70899
Dan Schultz <travelguy90@gmail.com>
2019-08-14 08:41:57 -0700

    Updates

---
+++ precache-manifest.922892ccbf40fc7cc200c7f3a8901270.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b46002c97daf418f3d15559d7cdaac13",
+    "url": "/best-selling-books/index.html"
+  },
+  {
+    "revision": "913d1bae4fe12dc942b2",
+    "url": "/best-selling-books/static/css/2.cc034990.chunk.css"
+  },
+  {
+    "revision": "2d436b5322771a70731f",
+    "url": "/best-selling-books/static/css/main.e053c617.chunk.css"
+  },
+  {
+    "revision": "913d1bae4fe12dc942b2",
+    "url": "/best-selling-books/static/js/2.ccd334da.chunk.js"
+  },
+  {
+    "revision": "b0499317f90a7143e80e",
+    "url": "/best-selling-books/static/js/3.f751221a.chunk.js"
+  },
+  {
+    "revision": "efddccfa15d42d5d107c",
+    "url": "/best-selling-books/static/js/4.160c61a3.chunk.js"
+  },
+  {
+    "revision": "683521bc12bafedd2b1b",
+    "url": "/best-selling-books/static/js/5.a378e189.chunk.js"
+  },
+  {
+    "revision": "2d436b5322771a70731f",
+    "url": "/best-selling-books/static/js/main.f797cd75.chunk.js"
+  },
+  {
+    "revision": "36df67edfe2cb4bd344d",
+    "url": "/best-selling-books/static/js/runtime~main.eac1ffb1.js"
+  },
+  {
+    "revision": "ab11e70ca52caa9aefdadf0c021ba5b9",
+    "url": "/best-selling-books/static/media/404.ab11e70c.png"
+  }
+]);
--- static/js/5.2ab7a2d7.chunk.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonpbest-selling-books"]=window["webpackJsonpbest-selling-books"]||[]).push([[5],{134:function(e,s,n){"use strict";n.r(s);var t=n(0),o=n.n(t);s.default=function(e){return o.a.createElement("div",{className:"book-container__week-list"},o.a.createElement("p",{className:"book-list-duration"},1!==e.wksOnLst?e.wksOnLst+" Weeks On List...":"New This Week"))}}}]);
-//# sourceMappingURL=5.2ab7a2d7.chunk.js.map
